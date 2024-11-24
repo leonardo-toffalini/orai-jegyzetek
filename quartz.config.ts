fc6c83a9dc85edf3b479e7588a4e0cf00cd75e5f
leonardo-toffalini <leonardotoffalini@gmail.com>
2024-11-24 21:08:33 +0100

    Quartz sync: Nov 24, 2024, 9:08 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Leo egyetemi jegyzetei",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
